a5a94803d94d813885e9de953015794017be022f
Kiizy <gregorycottet@live.fr>
2015-07-23 09:01:46 +0200

    erty

--- src/Equipe.java
+++ src/Equipe.java
@@ -1,13 +1,40 @@
 
 public class Equipe {
-	private int nbJoueurs;
-	private int nbRemplacants;
+	
+//ATTRIBUT
+	private int nbJoueurs=0;
+	private int nbRemplacants=0;
+	private static int nbEquipe;
 
+	
+	
+//CONSTRUCTEUR
 	public Equipe(int nbJoueurs) {
 		// TODO Auto-generated constructor stub
+		this.nbJoueurs = nbJoueurs;
+		nbEquipe++;
 	}
-	public Equipe(int nbJoueur, int nbRemplacants){
-		
+	public Equipe(int nbJoueurs, int nbRemplacants){
+		this.nbJoueurs = nbJoueurs;
+		this.nbRemplacants = nbRemplacants;
+		nbEquipe++;
+	}
+
+//METHODE
+	
+	
+//GETTERS-SETTERS
+	public int getNbJoueurs() {
+		return nbJoueurs;
+	}
+	public void setNbJoueurs(int nbJoueurs) {
+		this.nbJoueurs = nbJoueurs;
+	}
+	public int getNbRemplacants() {
+		return nbRemplacants;
+	}
+	public void setNbRemplacants(int nbRemplacants) {
+		this.nbRemplacants = nbRemplacants;
 	}
 
 }
